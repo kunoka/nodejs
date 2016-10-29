d9dcae39ac05ca975e64b102656f2adb6450e94d
harry <harry-git-command line tools>
2016-10-29 20:41:09 +0800

    add 2.3.1 codes 公共成员 和 私有成员 使用exports.property 和exports.function 变成公共成员 公共函数可以调用自己内部的私有function

--- 2/2.3/2.3.1/show.js
+++ 2/2.3/2.3.1/show.js
@@ -1,3 +1,12 @@
 /**
  * Created by harry on 10/29/16.
  */
+
+var Person = require('./module');
+console.log(Person.name);
+
+Person.init('Node.js');
+Person.show();
+Person.init('');
+Person.show();
+console.log(Person.myName);
